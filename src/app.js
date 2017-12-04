74cf2ea9d5e02e63cbb498bc6b9c197adb47d15d
Anusanthya Shanthakumar <anusanthya.shanthakumar@gmail.com>
2017-12-05 00:01:55 +0530

    Creates chart axes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-var margin = { top: 10, right: 20, bottom: 25, left: 25};
+var margin = { top: 10, right: 20, bottom: 60, left: 40};
 var width = 425 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
 
@@ -7,17 +7,29 @@ var svg = d3.select('.chart')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
   .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+    .attr('transform', 'translate('+ margin.left + ', ' + margin.top + ')');
 
 svg.append('rect')
-  .attr('width', width / 2)
+  .attr('width', width)
   .attr('height', height)
   .style('fill', 'lightblue')
   .style('stroke', 'green');
 
-svg.append('rect')
-  .attr('x', width / 2)
-  .attr('width', width / 2)
-  .attr('height', height)
-  .style('fill', 'lightblue')
-  .style('stroke', 'green');
+var yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0]);
+var yAxis = d3.axisLeft(yScale);
+svg.call(yAxis);
+
+var xScale = d3.scaleTime()
+  .domain([new Date(2017, 0, 1, 6), new Date(2017, 0, 1, 9)])
+  .range([0, width]);
+
+var xAxis = d3.axisBottom(xScale)
+  .ticks(5)
+  .tickSize(10)
+  .tickPadding(5);
+svg
+  .append('g')
+    .attr('transform', `translate(0, ${height})`)
+  .call(xAxis);
