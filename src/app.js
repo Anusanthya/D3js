359baf842751ee270d3b9b40bfca6a105c81b283
Anusanthya Shanthakumar <anusanthya.shanthakumar@gmail.com>
2017-12-05 21:07:20 +0530

    Reuse Transitions

--- src/app.js
+++ src/app.js
@@ -1,14 +1,47 @@
-d3.select('#block')
-  .transition()
-    .duration(600)
-    .delay(750)
-    .ease(d3.easeBounceOut)
-    .style('width', '400px')
-  .transition()
-    .duration(600)
-    .ease(d3.easeBounceOut)
-    .style('height', '600px')
-  .transition()
-    .duration(2000)
-    .ease(d3.easeQuadOut)
-    .style('background-color', 'turquoise')
+// var t = d3.transition()
+//   .delay(1000)
+//   .duration(1000);
+
+// d3.selectAll('.block')
+//   .transition(t)
+//   .style('width', '400px');
+
+// d3.select('.a')
+//   .transition(t)
+//   .style('background-color', 'lightgreen');
+
+// d3.select('.b')
+//   .transition(t)
+//   .style('background-color', 'brown');
+
+
+//OnClick events
+
+function go() {
+  var t = d3.transition()
+    .delay(1000)
+    .duration(1000);
+
+  d3.selectAll('.block')
+    .transition(t)
+    .style('width', '400px');
+
+  d3.select('.a')
+    .transition(t)
+    .style('background-color', 'lightgreen');
+
+  d3.select('.b')
+    .transition(t)
+    .style('background-color', 'brown');
+}
+
+function configure (t, delay, duration) {
+  return t.delay(delay).duration(duration);
+}
+
+function goNow() {
+  d3.selectAll('.block')
+    .transition()
+    .call(configure, 1000, 1000)
+    .style('height', '200px')
+}
