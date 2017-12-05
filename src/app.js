dcf7d8a4c899a8d4470a5b90b9a12f39bca64251
Anusanthya Shanthakumar <anusanthya.shanthakumar@gmail.com>
2017-12-05 20:38:23 +0530

    Animates transitions

--- src/app.js
+++ src/app.js
@@ -1,74 +1,14 @@
-var margin = { top: 10, right: 20, bottom: 30, left: 30};
-var width = 400 - margin.left - margin.right;
-var height = 565 - margin.top - margin.bottom;
-
-var svg = d3.select('.chart')
-  .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .call(responsivefy)
-  .append('g')
-    .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-d3.json('./data2.json', function (err, data) {
-
-  var xScale = d3.scaleLinear()
-    .domain([0, 100])
-    .range([0, width]);
-
-  svg
-    .append('g')
-      .attr('transform', `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
-
-  var yScale = d3.scaleLinear()
-    .domain([0, 100])
-    .range([height, 0]);
-
-  svg
-    .append('g')
-    .call(d3.axisLeft(yScale));
-
-  svg
-    .selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('cx', d => xScale(d.x))
-    .attr('cy', d => xScale(d.y))
-    .attr('r', d => d.r)
-    .attr('class', 'ball');
-});
-
-function responsivefy(svg) {
-  // get container + svg aspect ratio
-  var container = d3.select(svg.node().parentNode),
-      width = parseInt(svg.style("width")),
-      height = parseInt(svg.style("height")),
-      aspect = width / height;
-
-  // add viewBox and preserveAspectRatio properties,
-  // and call resize so that svg resizes on inital page load
-  svg.attr('viewBox', '0 0 ' + width + " " + height)
-      .attr("preserveAspectRatio", "xMinYMid")
-      .call(resize);
-
-  // to register multiple listeners for same event type,
-  // you need to add namespace, i.e., 'click.foo'
-  // necessary if you call invoke this function for multiple svgs
-  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
-  d3.select(window).on("resize." + container.attr("id"), resize);
-
-  // get width of container and resize svg to fit it
-  function resize() {
-    var targetWidth = parseInt(container.style("width"));
-    svg.attr("width", targetWidth);
-    svg.attr("height", Math.round(targetWidth / aspect));
-  }
-}
-
-//$0
-//$0.__data__
-//d3.select($0).attr('r', 50)
-//d3.selectAll('circle').filter(d => d.x < 30).nodes();
-//d3.selectAll('circle').filter(d => d.x < 30).style('stroke', 'red')
+d3.select('#block')
+  .transition()
+    .duration(600)
+    .delay(750)
+    .ease(d3.easeBounceOut)
+    .style('width', '400px')
+  .transition()
+    .duration(600)
+    .ease(d3.easeBounceOut)
+    .style('height', '600px')
+  .transition()
+    .duration(2000)
+    .ease(d3.easeQuadOut)
+    .style('background-color', 'turquoise')
